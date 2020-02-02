40d775b019d39e015e2a2291f9c1376cd1dc49d2
marcinurbaniak <marcin.urbaniak@gmail.com>
2020-02-02 20:39:05 +0100

    ReservationRepository -> corect method

--- src/main/java/com/urbande/springdemo/data/repository/ReservationRepository.java
+++ src/main/java/com/urbande/springdemo/data/repository/ReservationRepository.java
@@ -4,8 +4,10 @@ import com.urbande.springdemo.data.entity.Reservation;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Date;
+
 @Repository
 public interface ReservationRepository extends CrudRepository<Reservation, Long> {
 
-    Iterable<Reservation> findReservationByReservationDate();
+    Iterable<Reservation> findReservationByReservationDate(Date date);
 }
